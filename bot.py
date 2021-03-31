988d050bdbbfd12362230cc81bef3c75121fe30a
Achu2234 <75154223+Achu2234@users.noreply.github.com>
2021-03-31 11:58:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,6 @@ import string
 import traceback
 import random
 import asyncio
-from hachoir.parser import createParser
 from PIL import Image
 from datetime import datetime
 from random import choice
